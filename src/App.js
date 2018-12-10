3cde0ce0afe1fe66506dec98d9f5bcdd5442de95
santoshtechie9@gmail.com <September*09>
2018-12-09 19:15:12 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-//import './App.css';
 import DefaultLayout from './containers/MDBLayout/MDBLayout';
+import Login from './components/Forms/Login/Login';
+import Register from './components/Forms/Register';
+
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <DefaultLayout/>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={DefaultLayout}></Route>
+            <Route path="/login" exact component={Login}></Route>
+            <Route path="/register" exact component={Register}></Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/containers/MDBLayout/NavBarLayout.js
+++ src/containers/MDBLayout/NavBarLayout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Button, Container } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Button, Container, FormInline } from 'mdbreact';
+import { Link } from 'react-router-dom';
 import './MDBLayout.css';
 
 class NavbarFeatures extends Component {
@@ -19,42 +19,71 @@ class NavbarFeatures extends Component {
         });
     }
     render() {
-
         return (
-            <Router>
-                <Navbar  color="transparent" dark expand="md" fixed="top" scrolling>
-                    <Container>
+            <Navbar color="transparent" dark expand="md" fixed="top" scrolling>
+                <Container>
                     <NavbarBrand href="/">
-                        <strong>DLEARN</strong>
+                        <strong>D-LEARN</strong>
                     </NavbarBrand>
                     {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
                     <Collapse isOpen={this.state.collapse} color="blue" navbar>
                         <NavbarNav left>
                             <NavItem active>
-                                <NavLink className="nav-link" to="/"><strong>Home</strong></NavLink>
-                            </NavItem>
-                            <NavItem >
-                                <NavLink className="nav-link" to="/#popular-courses"><strong>Courses</strong></NavLink>
-                            </NavItem>
-                            <NavItem >
-                                <NavLink className="nav-link" to="/#contact"><strong>About us</strong></NavLink>
+                                <NavLink to="/courses">Courses</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to="/#contact"><strong>Contact us</strong></NavLink>
+                                <NavLink to="/blog">Blog</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink to="/contactus">Contact us</NavLink>
                             </NavItem>
                         </NavbarNav>
                         <NavbarNav right>
                             <NavItem>
-                                <Button color="white" >Login</Button>
+                                <Link to="/login"><Button color="white">Login</Button></Link>
                             </NavItem>
                             <NavItem>
-                                <Button color="primary" >Sign In</Button>
+                                <Link to="/register"><Button color="primary" >Sign Up</Button></Link>
+                            </NavItem>
+                            <NavItem>
+                                <FormInline waves>
+                                    <div className="md-form my-0">
+                                        <input
+                                            className="form-control mr-sm-2"
+                                            type="text"
+                                            placeholder="Search"
+                                            aria-label="Search"
+                                        />
+                                    </div>
+                                </FormInline>
                             </NavItem>
                         </NavbarNav>
+
+                        {/* <NavbarNav left>
+                                <NavItem active>
+                                    <NavLink className="nav-link" to="/"><strong>Home</strong></NavLink>
+                                </NavItem>
+                                <NavItem >
+                                    <NavLink className="nav-link" to="/popular-courses"><strong>Courses</strong></NavLink>
+                                </NavItem>
+                                <NavItem >
+                                    <NavLink className="nav-link" to="/#contact"><strong>About us</strong></NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/#contact"><strong>Contact us</strong></NavLink>
+                                </NavItem>
+                            </NavbarNav>
+                            <NavbarNav right>
+                                <NavItem>
+                                    <Link to="login"><Button color="white">Login</Button></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="register"><Button color="primary" >Sign Up</Button></Link>
+                                </NavItem>
+                            </NavbarNav> */}
                     </Collapse>
-                    </Container>
-                </Navbar>
-            </Router>
+                </Container>
+            </Navbar>
         );
     }
 }
